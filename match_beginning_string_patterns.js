c4e0e82b68627a9d6592d49b03ecfff4767480ed
jeffbai996 <j.bai@wustl.edu>
2022-11-08 10:46:13 -0800

    Added completed exercise code for Match Beginning String Patterns

--- match_beginning_string_patterns.js
+++ match_beginning_string_patterns.js
@@ -13,5 +13,5 @@
 // Use the caret character in a regex to find Cal only in the beginning of the string rickyAndCal.
 
 let rickyAndCal = "Cal and Ricky both like racing.";
-let calRegex = /change/; // Change this line
+let calRegex = /^Cal/; // Change this line
 let result = calRegex.test(rickyAndCal);
