13ab5025a27659b3e3ee1683aa7e0ac3d2b48c24
jeffbai996 <j.bai@wustl.edu>
2022-11-09 12:39:20 -0800

    Added completed exercise code for Match Ending String Patterns

--- match_ending_string_patterns.js
+++ match_ending_string_patterns.js
@@ -13,5 +13,5 @@
 // Use the anchor character ($) to match the string caboose at the end of the string caboose.
 
 let caboose = "The last car on a train is the caboose";
-let lastRegex = /change/; // Change this line
+let lastRegex = /caboose$/; // Change this line
 let result = lastRegex.test(caboose);
