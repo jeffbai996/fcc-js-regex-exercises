16a59d534d41c1cedd6caff63e542d2ffe664f96
jeffbai996 <j.bai@wustl.edu>
2022-11-09 12:45:24 -0800

    Added completed code for Match All Non-Numbers

--- match_all_nonnumbers.js
+++ match_all_nonnumbers.js
@@ -6,5 +6,5 @@
 // Use the shorthand character class for non-digits \D to count how many non-digits are in movie titles.
 
 let movieName = "2001: A Space Odyssey";
-let noNumRegex = /change/; // Change this line
+let noNumRegex = /\D/g; // Change this line
 let result = movieName.match(noNumRegex).length;
