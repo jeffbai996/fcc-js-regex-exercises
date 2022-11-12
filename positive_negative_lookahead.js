78ef1cbb47c8900eac35290d96e8a856127dce47
jeffbai996 <j.bai@wustl.edu>
2022-11-11 17:04:09 -0800

    Added completed exercise code for Positive and Negative Lookahead

--- positive_negative_lookahead.js
+++ positive_negative_lookahead.js
@@ -25,5 +25,5 @@
 // Use lookaheads in the pwRegex to match passwords that are greater than 5 characters long, and have two consecutive digits.
 
 let sampleWord = "astronaut";
-let pwRegex = /change/; // Change this line
+let pwRegex = /(?=\w{6})(?=\w*\d{2})/; // Change this line
 let result = pwRegex.test(sampleWord);
