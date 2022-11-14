93db25df8d3a0810a7d3d98985c03bee192630fb
jeffbai996 <j.bai@wustl.edu>
2022-11-14 12:07:48 -0800

    Added completed code for Check Mixed Grouping of Characters

--- check_mixed_grouping_characters.js
+++ check_mixed_grouping_characters.js
@@ -15,6 +15,6 @@
 // Then fix the code so that the regex that you have created is checked against myString and either true or false is returned depending on whether the regex matches.
 
 let myString = "Eleanor Roosevelt";
-let myRegex = /False/; // Change this line
-let result = false; // Change this line
+let myRegex = /(Franklin|Eleanor) (([A-Z]\.?|[A-Z][a-z]+) )?Roosevelt/; // Change this line
+let result = myRegex.test(myString); // Change this line
 // After passing the challenge experiment with myString and see how the grouping works
